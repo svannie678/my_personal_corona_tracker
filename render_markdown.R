c9a910d2e4cd31854f6d596e168abe73ecdb8cef
svannie678 <svannie678@gmail.com>
2020-12-16 12:05:00 -0500

    update

--- render_markdown.R
+++ render_markdown.R
@@ -5,3 +5,4 @@ library(rmarkdown)
 Sys.setenv(RSTUDIO_PANDOC='/Applications/RStudio.app/Contents/MacOS/pandoc')
 rmarkdown::render('/Users/simonetaylor/Desktop/corona_tracker/index.rmd', output_file='~/Desktop/corona_tracker/docs/index.html')
 
+
